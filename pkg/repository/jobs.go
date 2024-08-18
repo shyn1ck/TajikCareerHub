3bdfdf4797633599a009dcee90b1289535de6d82
shyn1ck <hy.parvizjon@gmail.com>
2024-08-18 18:01:16 +0500

    Add jobs repository functions, and user service

--- pkg/repository/jobs.go
+++ pkg/repository/jobs.go
@@ -1,1 +1,78 @@
 package repository
+
+import (
+	"TajikCareerHub/db"
+	"TajikCareerHub/models"
+	"log"
+)
+
+func GetAllJobs() (jobs []models.Job, err error) {
+	err = db.GetDBConn().Where("deleted_at = false").Find(&jobs).Error
+	if err != nil {
+		log.Printf("repository.GetAllJobs: Error retrieving all jobs. Error: %v\n", err)
+		return nil, err
+	}
+	log.Println("repository.GetAllJobs: Successfully retrieved all jobs.")
+	return jobs, nil
+}
+
+func GetJobByID(id uint) (job []models.Job, err error) {
+	err = db.GetDBConn().Where("id = ?", id).First(&job).Error
+	if err != nil {
+		log.Printf("repository.GetJob: Error retrieving job. Error: %v\n", err)
+		return nil, err
+	}
+	log.Println("repository.GetJob: Successfully retrieved job.")
+	return job, nil
+}
+
+func AddJob(job []models.Job) error {
+	log.Println("repository.AddJob: Adding new job to the database")
+	result := db.GetDBConn().Create(&job)
+	if result.Error != nil {
+		log.Printf("repository.AddJob: Failed to add job, error: %v\n", result.Error)
+		return result.Error
+	}
+	log.Println("repository.AddJob: Job added successfully")
+	return nil
+}
+
+func UpdateJob(jobID uint, job models.Job) (err error) {
+	log.Printf("repository.UpdateJob: Updating job with ID %v in the database...\n", jobID)
+	err = db.GetDBConn().Model(&models.Job{}).Where("id = ?", jobID).Updates(job).Error
+	if err != nil {
+		log.Printf("repository.UpdateJob: Failed to update job with ID %v. Error: %v\n", jobID, err)
+		return err
+	}
+	log.Printf("repository.UpdateJob: Job with ID %v updated successfully.\n", jobID)
+	return nil
+}
+
+func DeleteJob(jobID uint) (err error) {
+	log.Printf("repository.DeleteJob: Soft deleting job with ID %v...\n", jobID)
+	err = db.GetDBConn().Model(&models.Job{}).Where("id = ?", jobID).Update("is_deleted", true).Error
+	if err != nil {
+		log.Printf("repository.DeleteJob: Failed to soft delete job with ID %v. Error: %v\n", jobID, err)
+		return err
+	}
+	log.Printf("repository.DeleteJob: Job with ID %v successfully soft deleted.\n", jobID)
+	return nil
+}
+
+func FilterJobs(location string, category string) (jobs []models.Job, err error) {
+	log.Printf("repository.FilterJobs: Filtering jobs by location %v and category %v...\n", location, category)
+	query := db.GetDBConn()
+	if location != "" {
+		query = query.Where("location = ?", location)
+	}
+	if category != "" {
+		query = query.Where("category = ?", category)
+	}
+	err = query.Find(&jobs).Error
+	if err != nil {
+		log.Printf("repository.FilterJobs: Failed to filter jobs. Error: %v\n", err)
+		return nil, err
+	}
+	log.Printf("repository.FilterJobs: Successfully filtered jobs by location %v and category %v.\n", location, category)
+	return jobs, nil
+}
--- pkg/service/users.go
+++ pkg/service/users.go
@@ -1,1 +1,95 @@
 package service
+
+import (
+	"TajikCareerHub/models"
+	"TajikCareerHub/pkg/repository"
+	"TajikCareerHub/utils"
+	"errors"
+	"gorm.io/gorm"
+)
+
+func GetAllUsers() (users []models.User, err error) {
+	users, err = repository.GetAllUsers()
+	if err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func GetUserById(id uint) (user models.User, err error) {
+	user, err = repository.GetUserByID(id)
+	if err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func CreateUser(user models.User) error {
+	_, err := repository.GetUserByUserName(user.UserName)
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		return err
+	}
+	user.Password = utils.GenerateHash(user.Password)
+	err = repository.CreateUser(user)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func UpdateUser(user models.User) error {
+	err := repository.UpdateUser(user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteUser(id uint) error {
+	err := repository.DeleteUser(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateUserPassword(id uint, newPassword string) error {
+	hashedPassword := utils.GenerateHash(newPassword)
+	err := repository.UpdateUserPassword(id, hashedPassword)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func CheckUserExists(username, email string) (bool, error) {
+	exists, err := repository.CheckUserExists(username, email)
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
+}
+
+func GetUserByUsername(username string) (models.User, error) {
+	user, err := repository.GetUserByUserName(username)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return user, nil
+		}
+		return user, err
+	}
+	return user, nil
+}
+
+func GetUserByEmail(email string) (models.User, error) {
+	user, err := repository.GetUserByUserName(email)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return user, nil
+		}
+		return user, err
+	}
+	return user, nil
+}
